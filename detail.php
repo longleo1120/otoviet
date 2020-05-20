8cf41e258b866f0423437f14504cf38b99ac234a
= <=>
2020-05-20 16:43:31 +0700

    make it better

---
+++ detail.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<style>
+		a{
+			color: black;
+		}
+	</style>
+</head>
+<body style="background-color: #ebebeb">
+<?php 
+	include 'includes/header.php';
+ ?>
+	<main class="container">
+		<div class="block-left">
+			<div class="container">
+				<div class="breadcrumb-block">
+					<div class="left">
+						<ol class="breadcrumb search-breadcrumb">
+
+							<li class="breadcrumb-item"><a href="" title=""><i class="fas fa-long-arrow-alt-left"></i>Quay lại</a></li>
+							<li class="breadcrumb-item"><a href="" title="">Kawaraki</a></li>
+							<li class="breadcrumb-item"><a href="" title="">Z400</a></li>
+						</ol>
+
+					</div>
+				</div>
+
+				<div class="car-info-block">
+					<div class="row">
+						<div class="col-md-10">
+							<b>Xe Kawasaki Z400 2017 Xe mới Trong nước Tự động tại Thái Nguyên</b>
+							<p>Tin đăng bán Xe mới Kawasaki Z400</p>
+						</div>
+						<div class="col-md-2 text-right" >
+							<b>85 Triệu</b>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="col-8">
+							<h3 class="text-success">Nguyễn đăng Quang</h3>
+							<p>Hà Nội</p>
+						</div>
+						<div class="col-4 text-right">
+							<!-- Button to Open the Modal -->
+								<button type="button" class="btn btn-primary btn-success" data-toggle="modal" data-target="#showphone">
+								  Xem Số Điện Thoại
+								</button>
+
+								<!-- The Modal -->
+								<div class="modal" id="showphone">
+								  <div class="modal-dialog">
+								    <div class="modal-content">
+
+								      <!-- Modal Header -->
+								      <div class="modal-header">
+								      	 <button type="button" class="close"  style="float: top;" data-dismiss="modal">&times;</button>
+								      	<img class="image-avatar" src="resourse/image/10160.jpg" alt="">
+								        <h4><span>Nguyễn Đăng Quang</span></h4>
+								        <p>Cá Nhân</p>
+								        <p>Hà Nội</p>
+								       
+								      </div>
+
+								      <!-- Modal body -->
+								      <div class="modal-body">
+								      	<div class="row">
+								      		<div class="col-md-12 text-center">
+								      			<span>0123456789</span>
+								      			<p>Lưu ý không bao giờ gửi thanh toán trước</p>
+								      		</div>
+								      		<div class="col-md-12 text-center">
+								      			<button class="btn" style="background-color: #417ee1; color: white;">Gọi lại cho tôi</button>
+								      		</div>
+								      	</div>
+								      </div>
+
+								      
+
+								    </div>
+								  </div>
+								</div>
+						</div>
+						
+					</div>
+					<div class="row car-info-detail">
+						<div class="car-info-detail-left">
+							<img src="resourse/image/oto.jpg" alt="">
+						</div>
+						<div class="car-info-detail-right">
+							<div class="table-info-car-box">
+								<p>Thông số kỹ thuật xe</p>
+								<table class="table-info-car">
+				                        <tbody>
+				                        <tr>
+				                            <td class="text-secondary" width="105px">Hãng xe</td>
+				                            <td>Audi</td>
+				                        </tr>
+				                        <tr>
+				                            <td class="text-secondary">Model</td>
+				                            <td>Audi A5</td>
+				                        </tr>
+				                        <tr>
+				                            <td class="text-secondary">Năm sản xuất</td>
+				                            <td>2008</td>
+				                        </tr>
+				                        <tr>
+				                            <td class="text-secondary">Trạng thái</td>
+				                            <td>Xe cũ</td>
+				                        </tr>
+
+				                        <tr>
+				                            <td class="text-secondary">Màu xe</td>
+				                            <td>Trắng</td>
+				                        </tr>
+
+				                        <tr>
+				                            <td class="text-secondary">Màu nội thất</td>
+				                            <td>Đen</td>
+				                        </tr>
+
+				                        <tr>
+				                            <td class="text-secondary">Xuất xứ</td>
+				                            <td>Nhập khẩu</td>
+				                        </tr>
+
+				                        <tr>
+				                            <td class="text-secondary">Dòng xe</td>
+				                            <td>Coupe</td>
+				                        </tr>
+
+				                        <tr>
+				                            <td class="text-secondary">Hộp số</td>
+				                            <td>Tự động</td>
+				                        </tr>
+
+				                        <tr>
+				                            <td class="text-secondary">Dẫn động</td>
+				                            <td>4 bánh AWD</td>
+				                        </tr>
+
+				                        <tr>
+				                            <td class="text-secondary">Nhiên liệu</td>
+				                            <td>Xăng</td>
+				                        </tr>
+
+				                        <tr>
+				                            <td class="text-secondary">Số km đã đi</td>
+				                            <td>75,000 km</td>
+				                        </tr>
+				                    </tbody>
+                				</table>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class=" col-lg-12">
+							<div class="car-info-description">
+								<h5>Mô tả xe</h5>
+								<p>
+									Đăng ảnh không di chuyển được vị trí ảnh và xoá được ảnh khi đăng tin.
+									<br>
+									Cập nhật ảnh cho tin đăng bị crash và không hiển thị ảnh.
+								</p>
+							</div>
+							
+						</div>
+					</div>
+					
+				</div>
+				<div class="list-car">
+						<div class="row">
+							<div class="col-8">
+								<h4>Xe cùng hãng</h4>
+							</div>
+							<div class="col-4 text-right">
+								<div class="owl-nav disabled">
+									<button type="button" role="presentation" class="owl-prev disabled"><i class="fas fa-arrow-left"></i></button>
+									<button type="button" role="presentation" class="owl-next disabled"><i class="fas fa-arrow-right"></i></button></div>
+							</div>
+						</div>
+							
+						<div class="card-deck row">
+						  	<div class="card" style="width:400px">
+							  <img class="card-img-top" src="resourse/image/oto.jpg" alt="Card image">
+							  <div class="card-body">
+							    <h4 class="card-title"><a href="" title="">Xe audi xe cũ nhập khẩu</a></h4>
+							    <p class="card-text">500 Triệu</p>
+							  </div>
+							</div>
+						  	<div class="card" style="width:400px">
+							  <img class="card-img-top" src="resourse/image/oto.jpg" alt="Card image">
+							  <div class="card-body">
+							    <h4 class="card-title"><a href="" title="">Xe audi xe cũ nhập khẩu</a></h4>
+							    <p class="card-text">500 Triệu</p>
+							  </div>
+							</div>
+						  	<div class="card" style="width:400px">
+							  <img class="card-img-top" src="resourse/image/oto.jpg" alt="Card image">
+							  <div class="card-body">
+							    <h4 class="card-title"><a href="" title="">Xe audi xe cũ nhập khẩu</a></h4>
+							    <p class="card-text">500 Triệu</p>
+							  </div>
+							</div>
+							<div class="card" style="width:400px">
+							  <img class="card-img-top" src="resourse/image/oto.jpg" alt="Card image">
+							  <div class="card-body">
+							    <h4 class="card-title"><a href="" title="">Xe audi xe cũ nhập khẩu</a></h4>
+							    <p class="card-text">500 Triệu</p>
+							  </div>
+							</div>
+
+						  		
+						</div>
+				</div>
+			</div>
+		</div>
+		<div class="block-right" style="padding-top: 100px;">
+		<div class="widget-block widget-white">
+			Quảng cáo
+		</div>
+	</div>
+	</main>
+	
+<?php 
+	include 'includes/footer.php';
+ ?>
+</body>
+</html>
